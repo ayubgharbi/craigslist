7db2c43f94a1fbcc41ecf4e8c033cede3b612bc6
ayoub gharbi <ayoub97ma@gmail.com>
2020-04-24 20:25:21 +0000

    style forms

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,8 @@ module ApplicationHelper
     def is_home?
         controller.controller_name == "public" && controller.action_name == "home"
     end
+
+    def is_auth?
+        controller.controller_name == "sessions" || controller.controller_name == "registrations"
+    end
 end
